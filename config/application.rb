8c315e85a10d10f917771b32dab868d6f719c29f
Ee Shan <eeshansim@gmail.com>
2016-11-04 14:46:44 +0800

    move slack bot code into a class

--- config/application.rb
+++ config/application.rb
@@ -13,5 +13,12 @@ module PracticeBot
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.active_job.queue_adapter = :delayed_job
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
---
+++ lib/slack_bot_client.rb
@@ -0,0 +1,63 @@
+require 'slack-ruby-client'
+
+class SlackBotClient
+
+  def initialize(token)
+    Slack.configure do |config|
+      config.token = token
+      config.logger = Logger.new(STDOUT)
+      config.logger.level = Logger::INFO
+      fail 'Missing ENV[SLACK_API_TOKEN]!' unless config.token
+    end
+    start_client
+  end
+
+  def start_client
+    client = Slack::RealTime::Client.new
+
+    client.on :hello do
+      puts "Successfully connected, welcome '#{client.self.name}' to the '#{client.team.name}' team at https://#{client.team.domain}.slack.com."
+    end
+
+    client.on :close do |_data|
+      puts 'Connection closing, exiting.'
+    end
+
+    client.on :closed do |_data|
+      puts 'Connection has been disconnected.'
+    end
+
+    client.on :message do |data|
+      puts "Incoming message\n#{data}"
+
+      handle_message(data, client)
+    end
+
+    client.start!
+  end
+
+  def handle_message(data, client)
+    case data.text
+    when 'hi' then
+      client.web_client.chat_postMessage(
+        {
+          channel: data.channel,
+          text: "hello <@#{data.user}>!",
+          as_user: true,
+          attachments: [
+            {
+              color: COLORS["fs_green"],
+              title: "Never Enough!"
+            }
+          ]
+        })
+    when 'hello' then
+      client.web_client.chat_postMessage(
+        {
+          channel: data.channel,
+          text: "Sorry <@#{data.user}>, I don't understand you",
+          as_user: true
+        })
+    end
+  end
+end
